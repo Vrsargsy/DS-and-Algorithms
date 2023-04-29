9235fba6cc76ec0422d15dddbe423025484325c2
Vrsargsy <sargsyan.vruyr200023@gmail.com>
2023-04-30 00:17:55 +0400

    Simple quick sort without taking good pivot (average) formula

---
+++ Quick_sort.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+
+int	partition(vector<int> &arr, int i, int j)
+{
+	// int pivot = rand() % (i - j) + i; (best pivot == best speed) 
+	// swap(arr[pivot], arr[i]);
+	// pivot = i;
+	int	pivot = i;
+	int s1 = i;
+	int s2 = i;
+	for(int k = i + 1; k < j; ++k)
+	{
+		if (arr[k] >= arr[pivot])
+		{
+			++s2;
+		}
+		else
+		{
+			++s1;
+			swap(arr[s1], arr[k]);
+			++s2;
+		}
+	}
+	swap(arr[pivot], arr[s1]);
+	pivot = s1;
+	return pivot;
+}
+// O(n^2) if pivot is lowest number of array
+// O(n * log(n)) if pivot is good number (middle + - )
+// O(1) memory 
+void    quick_sort(vector<int> &arr, int i, int j)
+{
+	if (i == j)
+	{
+		return ;
+	}
+	int pivot = partition(arr, i, j);
+	quick_sort(arr, i, pivot);
+	quick_sort(arr, pivot + 1, j);
+}
+
+void    quick_sort(vector<int> &arr)
+{
+	quick_sort(arr, 0, arr.size());
+}
+
+void	print(vector<int> &arr)
+{
+	for(int i = 0; i < arr.size(); ++i)
+		cout << arr.at(i) << ' ';
+	cout << endl;
+}
+
+int main(void)
+{
+	vector<int> arr;
+	vector<int> copy_arr;
+	unsigned int arr_size = rand() % 10 + 1;
+	for(int i = 0; i < arr_size; ++i)
+		arr.push_back(rand() % 100 + 1);
+	copy_arr = arr;
+	quick_sort(arr);
+	
+	// sort(copy_arr.begin(), copy_arr.end());
+	// cout << "Is Sorted ? " << (copy_arr == arr) << endl; 
+
+}
